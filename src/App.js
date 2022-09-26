cda68bb165256c248783577faed42184751781c5
Hariono26 <hariono26011996@gmail.com>
2022-09-26 08:02:58 +0700

    minus edit feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Button, Form, Table } from "react-bootstrap";
+import { Button, Form, Table, Modal } from "react-bootstrap";
 import Axios from 'axios'
 import { useState, useRef } from 'react'
 
@@ -8,12 +8,15 @@ function App() {
   const [tokenData, setTokenData] = useState({})
   const [accLogin, setAcclogin] = useState(false)
   const [projectData, setProjectData] = useState([])
+  const [showModal, setShowModal] = useState(false)
   const [page, setPage] = useState(1)
   const [maxPage, setMaxPage] = useState(0)
   const [itemPerPage] = useState(5)
 
   const username = useRef()
   const password = useRef()
+  const projectDesc = useRef()
+  const projectName = useRef()
 
   const onLogin = () => {
     let userInput = username.current.value
@@ -53,8 +56,8 @@ function App() {
               <td>{item.description}</td>
               <td>{item.owner}</td>
               <td>
-                <Button variant='warning'>Edit</Button>
-                <Button variant='danger'>Hapus</Button>
+                <Button style={{marginRight:'10px'}} variant='warning' onClick={() => alert('Coming Soon')}>Edit</Button>
+                <Button variant='danger' onClick={() => onDelete(item.id)}>Hapus</Button>
               </td>
             </tr>
           )
@@ -64,17 +67,61 @@ function App() {
   }
 
   const onNext = () => {
-    setPage(page + 1 )
+    setPage(page + 1)
   }
 
   const onPrev = () => {
     setPage(page - 1)
   }
 
+  const onDelete = (id) => {
+    Axios.delete(`https://frontend-test-backend.tritronik.com/v1/projects/${id}`, { headers: { Authorization: `Bearer ${tokenData.access_token}` } })
+      .then(res => {
+        onShowProject(tokenData.access_token)
+      })
+  }
+
+  const onAdd = () => {
+    setShowModal(true)
+  }
+
+  const onAddOk = () => {
+    let newProjectData = {
+      "component": "string",
+      "description": `${projectDesc.current.value}`,
+      "id": 0,
+      "name": `${projectName.current.value}`,
+      "owner": "string",
+      "pages": [
+        {
+          "component": "string",
+          "description": "string",
+          "iconName": "string",
+          "id": 0,
+          "miniName": "string",
+          "name": "string",
+          "pageType": "DASHBOARD",
+          "path": "string",
+          "permission": "string",
+          "projectId": 0,
+          "title": "string",
+          "visibleInMenu": true
+        }
+      ],
+      "path": "string"
+    }
+    Axios.post(`https://frontend-test-backend.tritronik.com/v1/projects/`, newProjectData, { headers: { Authorization: `Bearer ${tokenData.access_token}` } })
+      .then(res => {
+        onShowProject(tokenData.access_token)
+      })
+    setShowModal(false)
+  }
+
   if (accLogin === true) {
     return (
       <div>
-        <Button variant='success' onClick={() => onShowProject(tokenData.access_token)}>Tampilkan Daftar Project</Button>
+        <Button variant='success' style={{marginRight:'10px'}} onClick={() => onShowProject(tokenData.access_token)}>Tampilkan Daftar Project</Button>
+        <Button variant='primary' disabled={maxPage === 0 ? true : false} onClick={onAdd}>Tambah Project Baru</Button>
         <Table bordered striped>
           <thead>
             <tr>
@@ -92,6 +139,34 @@ function App() {
           <div style={{ marginTop: 'auto', marginBottom: 'auto' }}>Page {page} of {maxPage}</div>
           <Button disabled={page >= maxPage ? true : false} onClick={onNext}>next</Button>
         </div>
+        <Modal
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          show={showModal}
+          onHide={() => setShowModal(false)}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Masukkan Data Project Baru
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>Input Nama Project</Form.Label>
+                <Form.Control type="text" placeholder="Masukkan Nama Project" defaultValue='new project' ref={projectName} />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Input Deskripsi</Form.Label>
+                <Form.Control type="text" placeholder="Masukkan Deskripsi" defaultValue='new description' ref={projectDesc} />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={onAddOk}>OK</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     )
   } else {
